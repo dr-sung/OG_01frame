abdf11b0b13c46cef2417b9138e4e03ef6c4b08a
Hong Sung <profsung@gmail.com>
2017-07-12 14:39:14 -0500

    Initial commit

---
+++ src/EmptyFrame.java
@@ -0,0 +1,14 @@
+
+import javax.swing.JFrame;
+
+
+public class EmptyFrame extends JFrame {
+    public EmptyFrame() {
+        
+    }
+    public void initialize() {
+        setTitle("Frame Test");
+        setSize(300, 300); // width height in pixels
+        setLocation(100, 100); // origin: upper-left corner of screen
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+
+import javax.swing.JFrame;
+
+
+public class Main {
+        public static void main(String[] args) {
+        EmptyFrame frame = new EmptyFrame();
+        frame.initialize();
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
